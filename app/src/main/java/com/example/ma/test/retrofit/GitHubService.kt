a397b88326c31dcecd07a8f9e77afe1340452e42
mazongkun <14040486@qq.com>
2021-06-23 15:01:32 +0800

    init

---
+++ app/src/main/java/com/example/ma/test/retrofit/GitHubService.kt
@@ -0,0 +1,12 @@
+package com.example.ma.test.retrofit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+//    @GET("users/{user}/repos")
+//  Call<List<Repo>> listRepos(@Path("user") String user);
+    @GET("users/{user}/repos")
+    fun listRepos(@Path("user") user: String) : Call<List<Repo>>
+}
